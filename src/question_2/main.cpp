3f3c67ead2a4f8ee318d3f6adbcc15e7ef381265
Tyler <minhphu.pham@g.austincc.edu>
2024-10-20 02:33:19 +0000

    question2

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,7 +1,32 @@
 #include "question2.h"
 
+using std::cin; using std::cout;
+
 int main()
-{
+{   
+    string str; bool result; char answer;
+
+    do
+    {
+        cout << "***This program will check if a string is palindrome or not.***\n";
+        cout << "Please enter a string - ";
+        cin >> str;
+        result = is_palindrome(str);
+
+        if (result == true)
+            cout << "True.\n" << str << " is a palindrome string.\n";
+        else
+            cout << "False.\n" << str << " is not a palindrome string.\n";
     
+        cout << "Do you want to continue?(y/n) - ";
+        cin >> answer;
+        
+        while (toupper(answer) != 'Y' && toupper(answer) != 'N')
+        {
+            cout << "Only 'y' or 'n'. Please try again.\n";
+            cout << "Do you want to continue?(y/n) - ";
+            cin >> answer;
+        }
+    }while (toupper(answer) != 'N');
     return 0;
 }
